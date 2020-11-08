45358f80247b2a2dfef7a3a54430785fb33d9a5c
Eli <epugh@stanford.edu>
2020-11-08 01:05:54 -0500

    first

---
+++ rlcard/envs/euchre.py
@@ -0,0 +1,26 @@
+from rlcard.envs import Env
+from rlcard.games.euchre import Game
+from rlcard.utils.euchre_utils import ACTION_SPACE, ACTION_LIST
+
+class EuchreEnv(Env):
+
+    def __init__(self, config):
+        self.game = Game()
+        self.name = "euchre"
+        super().__init__(config)
+
+    def _extract_state(self, state):
+        state['legal_actions'] = self._get_legal_actions()
+        state['raw_legal_actions'] = self.game.get_legal_actions()
+        return state
+
+    def _decode_action(self, action_id):
+        return ACTION_LIST[action_id]
+
+    def _get_legal_actions(self):
+        legal_actions = self.game.get_legal_actions()
+        legal_ids = [ACTION_SPACE[action] for action in legal_actions]
+        return legal_ids
+
+    def get_payoffs(self):
+        return self.game.get_payoffs()
